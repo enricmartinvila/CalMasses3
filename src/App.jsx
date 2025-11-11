13a819e36bbada43a1afa56f4e6d8203e5d37afd
enricMercura <emv@mercura.dk>
2025-11-11 13:05:18 +0100

    delete unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import './Pages/MainPage'
 import MainPage from './Pages/MainPage'
